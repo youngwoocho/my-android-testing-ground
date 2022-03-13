a0f2fb38e7ee8d1da5519fce979d74dff52ab6ad
조영우 <youngchorg@gmail.com>
2022-03-13 23:00:10 +0900

    Add previewView and textview

--- camerafocustest/src/main/java/com/example/camerafocustest/MainActivity.kt
+++ camerafocustest/src/main/java/com/example/camerafocustest/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.camerafocustest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.camerafocustest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        updateSupportAutoFocusText("")
+
+    }
+
+    private fun updateSupportAutoFocusText(text: String) {
+        viewBinding.textViewSupportAutofocus.text =
+            getString(R.string.text_view_support_autofocus, text)
+
     }
 }

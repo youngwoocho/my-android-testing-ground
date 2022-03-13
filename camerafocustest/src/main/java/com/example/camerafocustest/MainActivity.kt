29824b38520f71b0e2de2eabe88881d1bf1c485b
조영우 <youngchorg@gmail.com>
2022-03-13 22:40:33 +0900

    Add viewBinding

--- camerafocustest/src/main/java/com/example/camerafocustest/MainActivity.kt
+++ camerafocustest/src/main/java/com/example/camerafocustest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.camerafocustest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.camerafocustest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }

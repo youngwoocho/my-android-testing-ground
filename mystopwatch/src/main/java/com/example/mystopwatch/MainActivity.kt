ac0e31d9e71e4d0835f0937a3e386e14b91742fa
조영우 <youngchorg@gmail.com>
2022-04-01 07:12:54 +0900

    Add layout for StopWatch app

---
+++ mystopwatch/src/main/java/com/example/mystopwatch/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mystopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mystopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding : ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+}
